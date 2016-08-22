c9dc0a22690692458011c598c3ea97a00a5c0263
Greg Smith <crashingwaves@fea.st>
2016-08-21 22:32:31 -0600

    Display weather correctly

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,44 @@
-var $ = require('jquery');
+import $ from 'jquery';
+import handlebars from 'handlebars';
 
 $(document).ready(() => {
-})
+    const forecastTemplate = handlebars.compile($('#future-template').html());
+    if ("geolocation" in navigator) {
+        getPositionData().then((position) => {
+            const coords = `${position.coords.latitude},${position.coords.longitude}`;
+            return getWeatherData(coords);
+        }).then((data) => {
+            console.log(data);
+            $('.current-location').html(data.city);
+            $('.description').html(data.currentWeather.summary);
+            $('.current').html(data.currentWeather.temperature);
+            data.nextWeekWeather.forEach((forecast) => {
+                $('.five-day').append(forecastTemplate(forecast))
+            })
+        }).catch((error) => {
+            // display error
+        });
+    } else {
+        // show geolocation error
+    }
+});
+
+function getWeatherData(coords) {
+    return $.ajax({
+        method: "POST",
+        url: "http://localhost:9000/weather",
+        data: JSON.stringify({
+            "currentPosition": coords
+        }),
+        "dataType": "json",
+        "contentType": "application/json"
+    });
+}
+
+function getPositionData() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+}
+
+// need a function to change temperature units
--- src/tests/main_spec.js
+++ src/tests/main_spec.js
@@ -1,6 +1,6 @@
 import test from 'tape';
 
-test('another simple fucking test', (t) => {
-    t.pass('fuck you');
+test('', (t) => {
+
     t.end();
 })
