70bc489ee9fd0a474bea6cebccb3a92a9678584f
Gregory Smith <crashingwaves@fea.st>
2016-08-24 13:17:10 -0600

    Switch current weather to template

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,43 +2,45 @@ import $ from 'jquery';
 import handlebars from 'handlebars';
 
 $(document).ready(() => {
-    const forecastTemplate = handlebars.compile($('#future-template').html());
-    if ("geolocation" in navigator) {
-        getPositionData().then((position) => {
-            const coords = `${position.coords.latitude},${position.coords.longitude}`;
-            return getWeatherData(coords);
-        }).then((data) => {
-            console.log(data);
-            $('.current-location').html(data.city);
-            $('.description').html(data.currentWeather.summary);
-            $('.current').html(data.currentWeather.temperature);
-            data.nextWeekWeather.forEach((forecast) => {
-                $('.five-day').append(forecastTemplate(forecast))
-            })
-        }).catch((error) => {
-            // display error
-        });
-    } else {
-        // show geolocation error
-    }
+  const currentTemplate = handlebars.compile($('#current-template').html()),
+   forecastTemplate = handlebars.compile($('#future-template').html());
+
+  if ("geolocation" in navigator) {
+    getPositionData().then((position) => {
+      const coords = `${position.coords.latitude},${position.coords.longitude}`;
+      return getWeatherData(coords);
+    }).then((data) => {
+        console.log(data);
+        $('.current-location').html(data.city);
+        $('.today').append(currentTemplate(data.currentWeather));
+
+      data.nextWeekWeather.forEach((forecast) => {
+        $('.five-day').append(forecastTemplate(forecast))
+      })
+    }).catch((error) => {
+      // display error
+    });
+  } else {
+    // show geolocation error
+  }
 });
 
 function getWeatherData(coords) {
-    return $.ajax({
-        method: "POST",
-        url: "http://localhost:9000/weather",
-        data: JSON.stringify({
-            "currentPosition": coords
-        }),
-        "dataType": "json",
-        "contentType": "application/json"
-    });
+  return $.ajax({
+    method: "POST",
+    url: "http://localhost:9000/weather",
+    data: JSON.stringify({
+      "currentPosition": coords
+    }),
+    "dataType": "json",
+    "contentType": "application/json"
+  });
 }
 
 function getPositionData() {
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
 }
 
 // need a function to change temperature units
