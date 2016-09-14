eb0b206d992e610b2dc2197e63b4a7be4dac2417
Gregory Smith <crashingwaves@fea.st>
2016-09-14 07:31:30 -0600

    Refactor to RxJS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,62 +1,50 @@
 import $ from 'jquery';
 import handlebars from 'handlebars';
+import Rx, { Observable } from 'rxjs';
+import { DOM } from 'rx-dom';
 
-$(document).ready(() => {
-  const currentTemplate = handlebars.compile($('#current-template').html()),
+const currentTemplate = handlebars.compile($('#current-template').html()),
    forecastTemplate = handlebars.compile($('#future-template').html()),
    errorTemplate = handlebars.compile($('#error-template').html());
 
-   $('#scale-btn').click(() => {
-        $('.temp').each((index, $item) => {
-            $item = $($item);
-            if($item.hasClass('farenheit')) {
-                $item.removeClass('farenheit');
-                $item.addClass('celsius');
-                const temp = $item.html();
-                $item.html(((temp - 32)/1.8).toFixed(2));
-            } else if($item.hasClass('celsius')) {
-                $item.removeClass('celsius');
-                $item.addClass('farenheit');
-                const temp = $item.html();
-                $item.html(((temp * (1.8)) + 32).toFixed(2));
-            }
-        });
-      });
-
-  if ("geolocation" in navigator) {
-    getPositionData().then((position) => {
-      const coords = `${position.coords.latitude},${position.coords.longitude}`;
-      return getWeatherData(coords);
-    }).then((data) => {
-        console.log(data);
-        $('.current-location').html(data.city);
-        $('.today').append(currentTemplate(data.currentWeather));
-
-      data.nextWeekWeather.forEach((forecast) => {
-        $('.five-day').append(forecastTemplate(forecast))
-      });
-    }).catch((error) => {
-      // display error
-      $('.jumbotron').html(errorTemplate(error));
-      $('#scale-btn').hide();
-    });
+DOM.ready()
+.flatMap(() => DOM.geolocation.getCurrentPosition({enableHighAccuracy: true}))
+.flatMap(data => DOM.ajax({
+  url: "http://localhost:9000/weather",
+  method: "POST",
+  body: JSON.stringify({"currentPosition": `${data.coords.latitude},${data.coords.longitude}`}),
+  responseType: 'json',
+  headers: {
+    'content-type': 'application/json'
   }
+}))
+.map(data => data.response)
+.subscribe(data => {
+  $('.current-location').html(data.city);
+  $('.today').append(currentTemplate(data.currentWeather));
+
+  data.nextWeekWeather.forEach((forecast) => {
+    $('.five-day').append(forecastTemplate(forecast))
+  });
+}, (error) => {
+  // display error
+  $('.jumbotron').html(errorTemplate(error));
+  $('#scale-btn').hide();
 });
 
-function getWeatherData(coords) {
-  return $.ajax({
-    method: "POST",
-    url: "http://localhost:9000/weather",
-    data: JSON.stringify({
-      "currentPosition": coords
-    }),
-    "dataType": "json",
-    "contentType": "application/json"
+Observable.fromEvent($('#scale-btn'), 'click').subscribe(() => {
+  $('.temp').each((index, $item) => {
+      $item = $($item);
+      if($item.hasClass('farenheit')) {
+          $item.removeClass('farenheit');
+          $item.addClass('celsius');
+          const temp = $item.html();
+          $item.html(((temp - 32)/1.8).toFixed(2));
+      } else if($item.hasClass('celsius')) {
+          $item.removeClass('celsius');
+          $item.addClass('farenheit');
+          const temp = $item.html();
+          $item.html(((temp * (1.8)) + 32).toFixed(2));
+      }
   });
-}
-
-function getPositionData() {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-}
+})
